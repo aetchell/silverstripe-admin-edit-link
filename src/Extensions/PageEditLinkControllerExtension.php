58486dc8fdc21e5c00a7e3ecdf2de17e96529504
Ashley Etchell <aetchell@gmail.com>
2022-04-01 12:07:29 +1300

    Update PageEditLinkControllerExtension.php
    Clean up code in Extension file

--- src/Extensions/PageEditLinkControllerExtension.php
+++ src/Extensions/PageEditLinkControllerExtension.php
@@ -5,12 +5,10 @@ namespace aetchell\Extensions;
 use SilverStripe\ORM\DataExtension;
 use SilverStripe\View\Requirements;
 
-class PageEditLinkControllerExtension extends DataExtension {
-
-    /**
-     * Test if this page has an override theme set from within the settings tab, if so, rebuild the theme registry
-     */
-    public function onAfterInit() { 
+class PageEditLinkControllerExtension extends DataExtension 
+{
+    public function onAfterInit() 
+    { 
         Requirements::javascript('aetchell/silverstripe-admin-edit-link:client/js/script.js', ['defer' => true]);
         Requirements::css('aetchell/silverstripe-admin-edit-link:client/css/EditLink.css');
     }
